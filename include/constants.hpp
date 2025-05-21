9af9d459cd3bcb730f234fbd6c1d982c010abcd4
Jose Padilla <59151555+A1DS19@users.noreply.github.com>
2025-05-21 14:48:49 -0700

    render window

---
+++ include/constants.hpp
@@ -0,0 +1,7 @@
+#pragma once
+
+namespace constants {
+constexpr int window_w = 520;
+constexpr int window_h = 520;
+constexpr int framerate = 60;
+} // namespace constants
--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,25 @@
 #include <stdlib.h>
 
-int main() { return EXIT_SUCCESS; }
+#include <SFML/Graphics.hpp>
+
+#include "constants.hpp"
+
+int main() {
+  sf::RenderWindow game_window{
+      sf::VideoMode({constants::window_w, constants::window_h}), "Breakout"};
+
+  game_window.setFramerateLimit(constants::framerate);
+
+  while (game_window.isOpen()) {
+    while (const std::optional event = game_window.pollEvent()) {
+      if (event->is<sf::Event::Closed>()) {
+        game_window.close();
+      }
+    }
+
+    game_window.clear(sf::Color::Black);
+    game_window.display();
+  }
+
+  return EXIT_SUCCESS;
+}
